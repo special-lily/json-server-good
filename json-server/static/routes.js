c861879b61c14a30c3496128f229259317f3cd75
yangjiali <yangjiali@jd.com>
2018-04-24 18:47:14 +0800

    add json-server

---
+++ json-server/static/routes.js
@@ -0,0 +1,8 @@
+module.exports= {
+    "/member/listEquities": "/rightsList",
+    "/login/member": "/cardData",
+    "/login/member/barCode": "/barCode",
+    "/login/member/encryCode" : "/encryCode",
+    "/login/member/checkVerified" : "/checkVerified",
+    "/member/listMemberGoods" : "/listMemberGoods"
+}
